6a4e93a756fc7d0f7b4afc5a2db1e849fe277ca1
prasadm-01 <prasadmore460@gmail.com>
2026-06-27 15:27:32 +0530

    Add files via upload

---
+++ src/components/Footer.jsx
@@ -0,0 +1,18 @@
+import './Footer.css'
+
+
+const Footer = () => {
+    return (
+        <div className="footerWeb">
+            <footer>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, culpa!</p>
+            </footer>
+            <div>
+                Copyright to Prasad's Parlour
+            </div>
+        </div>
+    )
+}
+
+
+export default Footer
